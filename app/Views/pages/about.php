b1557646a60138f38700e168ed634f5a2a234b3a
darulannas <darulannas7@gmail.com>
2023-01-09 22:04:55 +0700

    views

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam eius sed repudiandae alias magnam, perspiciatis sint, iste rerum, obcaecati nostrum repellendus voluptatibus nisi asperiores optio quisquam exercitationem eligendi minima molestiae!</p>
+        </div>
+    </div>
+</div>
